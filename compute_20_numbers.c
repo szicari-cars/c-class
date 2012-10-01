d93eaec786c349559d530ca5134bd25a4e4b2861
Sean Zicari <szicari@cars.com>
2012-10-01 09:02:34 -0700

    I think compute_20_numbers.c is ready to go.

--- compute_20_numbers.c
+++ compute_20_numbers.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-double compute_smallest_number(double, double);
+double compute_smallest_number(double, double, int);
 double compute_largest_number(double, double);
 double calculate_average(double, int);
-bool is_digit(int);
 void clear_buffer();
+void print_greeting(int);
+void print_statistics(double, int, double, double);
 
+/**
+ * Ask for 20 numbers in a loop. When 20 valid numbers are entered, calculate
+ * the average and report out the largest, smallest and average numbers.
+ *
+ * Control the loop by controlling the counter alone, letting the loop worry
+ * about itself.
+ */
 int main()
 {
   int i, quantity = 20;
   double number, running_sum, largest_number, smallest_number;
 
-  printf("I am going to ask you for %d numbers, one at a time. ", quantity);
-  printf("Once you've entered the 20 numbers, I'll give you the average of ");
-  printf("those numbers and also tell you the biggest and smallest.\n\n");
+  print_greeting(quantity);
 
   for (i = 1; i <= quantity; i += 1)
   {
@@ -23,7 +28,7 @@ int main()
     if (scanf("%lf", &number))
     {
       running_sum += number;
-      smallest_number = compute_smallest_number(number, smallest_number);
+      smallest_number = compute_smallest_number(number, smallest_number, i);
       largest_number = compute_largest_number(number, largest_number);
     }
     else
@@ -36,21 +41,29 @@ int main()
     clear_buffer();
   }
 
-  printf("\n\nYour average is %.2lf.\n", calculate_average(running_sum, quantity));
-  printf("The smallest number you entered was %.2lf.\n", smallest_number);
-  printf("The largest number you entered was %.2lf.\n", largest_number);
+  print_statistics(running_sum, quantity, smallest_number, largest_number);
 
   return 0;
 }
 
-bool is_digit(int number)
+void print_greeting(int quantity)
 {
-  return (number >= 48 && number <= 57);
+  printf("I am going to ask you for %d numbers, one at a time. ", quantity);
+  printf("Once you've entered the 20 numbers, I'll give you the average of ");
+  printf("those numbers and also tell you the biggest and smallest.\n\n");
+}
+
+void print_statistics(double running_sum, int quantity,
+                      double smallest_number, double largest_number)
+{
+  printf("\n\nYour average is %.2lf.\n", calculate_average(running_sum, quantity));
+  printf("The smallest number you entered was %.2lf.\n", smallest_number);
+  printf("The largest number you entered was %.2lf.\n", largest_number);
 }
 
 void clear_buffer()
 {
-  while (getchar());
+  while (getchar() != '\n');
 }
 
 double calculate_average(double total, int quantity)
@@ -65,9 +78,11 @@ double compute_largest_number(double number, double largest_number)
   return largest_number;
 }
 
-double compute_smallest_number(double number, double smallest_number)
+double compute_smallest_number(double number, double smallest_number, int iteration)
 {
-  if (number < smallest_number) smallest_number = number;
+  // Keep the current number if it's the smallest or if we're on the first
+  // iteration through the loop.
+  if (iteration == 1 || number < smallest_number) smallest_number = number;
 
   return smallest_number;
 }
