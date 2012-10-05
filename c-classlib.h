acbebd67fa6f2dfaf123634b11b79c919255cffe
Sean Zicari <szicari@cars.com>
2012-10-05 10:56:25 -0700

    Finished first pass on number sequence iterator and example script.

--- c-classlib.h
+++ c-classlib.h
@@ -1,3 +1,7 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdbool.h>
+
 /**
  * Given any size integer, will return each digit in sequence. Written for use
  * in while loops.
@@ -7,14 +11,29 @@
  *
  * @return int The next digit in the sequence
  */
-int get_digit(int digit, int *input_number)
+int get_digit(int *input_number)
 {
-    int num_digits = 0, temp_digit = 0;
+    int digit = 0,
+        num_digits = 0,
+        temp_digit = *input_number;
 
-    while (temp_digit / 10 > 0)
+    while ((temp_digit = temp_digit / 10) > 0)
     {
         digit = temp_digit;
         num_digits += 1;
     }
+
+    // We still have a valid number, but it's in the 10's place.
+    if (num_digits == 0)
+    {
+        digit = *input_number;
+        *input_number = false;
+    }
+    else
+    {
+        *input_number = *input_number % (num_digits * (int)pow(10, num_digits));
+    }
+
+    return digit;
 }
 
