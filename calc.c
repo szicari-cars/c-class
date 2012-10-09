b0fb158eafd32749ccf2cbeda9fca2bd56538bdf
Sean Zicari <szicari@cars.com>
2012-10-09 08:51:00 -0700

    wip: The calculator...needs some work.

--- calc.c
+++ calc.c
@@ -1,52 +1,62 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool parse_input(char, double, double *);
+bool parse_input(char *);
+bool do_input(char *input);
 
 int main()
 {
-    double result = 0.0, number_input = 0.0;
-    char command;
+    double result = 0.0;
+    char *input;
     bool stop_calculating = false;
 
     printf("Calculator is on.\n");
 
     while (stop_calculating == false)
     {
-        printf("Please enter a number: ");
-        scanf("%c", &command);
-        scanf("%lf", &number_input);
-        stop_calculating = parse_input(command, number_input, &result);
+        printf("Please enter your input: ");
+        scanf("%s", input);
+        stop_calculating = parse_input(input);
     }
 }
 
-bool parse_input(char command, double number_input, double *result)
+bool do_input(char *input)
 {
     bool stop_calculating = false;
 
     printf("\n");
 
-    switch (command)
-    {
-        case 'h':
-        case 'H':
-            printf("Help goes here.\n");
-            break;
+    //switch (input)
+    //{
+    //    case 'h':
+    //    case 'H':
+    //        printf("Help goes here.\n");
+    //        break;
 
-        case 'r':
-        case 'R':
-            printf("Result is shown and the calculation cleared.\n");
-            break;
+    //    case 'r':
+    //    case 'R':
+    //        printf("Result is shown and the calculation cleared.\n");
+    //        break;
 
-        case 'q':
-        case 'Q':
-            stop_calculating = true;
-            break;
+    //    case 'q':
+    //    case 'Q':
+    //        stop_calculating = true;
+    //        break;
 
-        default:
-            printf("UnknownOperatorException is thrown.\n");
-    }
+    //    default:
+    //        printf("UnknownOperatorException is thrown.\n");
+    //}
 
     return stop_calculating;
+}
+
+/**
+ * Scan the input string for commands to perform.
+ */
+bool parse_input(char *input)
+{
+    char command;
+    double input_number;
+    printf(input);
 }
 
