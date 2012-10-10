f875b319a3db44e1381c3133c56d617426d829ae
Sean Zicari <szicari@cars.com>
2012-10-09 23:02:39 -0700

    Play lottery is working!

--- play_lottery.c
+++ play_lottery.c
@@ -15,56 +15,54 @@ int play_lottery(int);
 void print_lottery_results(int);
 void show_greeting();
 bool valid_lottery_number(int);
+void split_number(int, int *, int *);
 
 int main()
 {
-  int entered_number, lottery_number, prize;
+    int entered_number, lottery_number, prize;
 
-  show_greeting();
+    show_greeting();
 
-  printf("Please enter a two-digit number: ");
-  scanf("%d", &entered_number);
+    printf("Please enter a two-digit number: ");
+    scanf("%d", &entered_number);
 
-  if (valid_lottery_number(entered_number))
-  {
-    prize = play_lottery(entered_number);
-    print_lottery_results(prize);
-  }
-  else
-  {
-    printf("%d is not a valid lottery number. You have three minutes to live.\n", entered_number);
-  }
+    if (valid_lottery_number(entered_number))
+    {
+        prize = play_lottery(entered_number);
+        print_lottery_results(prize);
+    }
+    else
+    {
+        printf("%d is not a valid lottery number. You have three minutes to live.\n", entered_number);
+    }
 }
 
 int play_lottery(int entered_number)
 {
-  int lottery_number = generate_lottery_number();
-  printf("The lottery number is %d.\n", lottery_number);
-  return determine_prize(entered_number, lottery_number);
+    int lottery_number = generate_lottery_number();
+    printf("The lottery number is %d.\n", lottery_number);
+    return determine_prize(entered_number, lottery_number);
 }
 
 bool valid_lottery_number(int entered_number)
 {
-  bool valid = true;
-  int number_length = 0;
+    bool valid = true;
+    int number_length = 0;
 
-  while (entered_number > 0)
-  {
-    entered_number /= 10;
-    number_length += 1;
-  }
-  if (number_length > LOTTO_NUMBER_LENGTH) valid = false;
+    while (entered_number > 0)
+    {
+        entered_number /= 10;
+        number_length += 1;
+    }
 
-  // TODO: Validate individual digits
-  return valid;
+    return (number_length == LOTTO_NUMBER_LENGTH);
 }
 
 void show_greeting()
 {
-  printf("Welcome to the lottery!\n\n");
-  printf("Enter a two-digit number to win big money.\n");
-  printf("PRIZES:\nExact match:\t\t$10,000\nMatch both digits:\t$ 3,000\nMatch one digit:\t$ 1,000\n\n");
-  printf("Please note that only the first two digits will be accepted.\n");
+    printf("Welcome to the lottery!\n\n");
+    printf("Enter a two-digit number to win big money.\n");
+    printf("------\nPRIZES\n------\nExact match:\t\t$10,000\nMatch both digits:\t$ 3,000\nMatch one digit:\t$ 1,000\n\n");
 }
 
 /**
@@ -72,20 +70,44 @@ void show_greeting()
  */
 int generate_lottery_number()
 {
-  srand(time(NULL));
-  return (rand() % 100) + 10;
+    srand(time(NULL));
+    return (rand() % 100) + 10;
 }
 
 int determine_prize(int entered_number, int lottery_number)
 {
-  return GRAND_PRIZE;
+    int guess1, guess2, lotto1, lotto2, prize = NO_PRIZE;
+
+    split_number(entered_number, &guess1, &guess2);
+    split_number(lottery_number, &lotto1, &lotto2);
+
+    // They guessed both numbers in the right order
+    if ((guess1 - lotto1 == 0) && (guess2 - lotto2 == 0))
+        prize = GRAND_PRIZE;
+
+    // They guessed both numbers in the opposite order
+    else if ((guess1 - lotto2 == 0) && (guess2 - lotto1 == 0))
+        prize = BIG_PRIZE;
+
+    // They guessed at least one number in any order
+    else if ((guess1 - lotto1 == 0) || (guess2 - lotto2 == 0) ||
+        (guess1 - lotto2 == 0) || (guess2 - lotto1 == 0))
+        prize = SMALL_PRIZE;
+
+    return prize;
+}
+
+void split_number(int number, int *split1, int *split2)
+{
+    *split1 = number / 10;
+    *split2 = number % 10;
 }
 
 void print_lottery_results(int prize)
 {
-  if (prize == GRAND_PRIZE) printf("Congratulations!! You won $10,000!\n");
-  if (prize == BIG_PRIZE) printf("Really good! You won $3,000!\n");
-  if (prize == SMALL_PRIZE) printf("Not bad! You won $1,000!\n");
-  else if (prize == NO_PRIZE) printf("Wow, you suck. You won NOTHING.\n");
+    if (prize == GRAND_PRIZE) printf("Congratulations!! You won $10,000!\n");
+    if (prize == BIG_PRIZE) printf("Really good! You won $3,000!\n");
+    if (prize == SMALL_PRIZE) printf("Not bad! You won $1,000!\n");
+    else if (prize == NO_PRIZE) printf("Wow, you suck. You won NOTHING.\n");
 }
 
