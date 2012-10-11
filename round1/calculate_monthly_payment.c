e71b74153e81f872cb2324abe192f38665bd9c1e
Sean Zicari <szicari@cars.com>
2012-10-11 15:23:16 -0700

    Committing working monthly payment calculator, and unfinished serial number validator.

--- round1/calculate_monthly_payment.c
+++ round1/calculate_monthly_payment.c
@@ -4,13 +4,14 @@
 void display_greeting();
 double calculate_monthly_payment(double, double, int);
 double calculate_interest_percent(double);
+double calculate_total_paid(double monthly_payment, int years);
 
 /**
  * You *know* what this is.
  */
 int main()
 {
-    double interest_rate, principle, monthly_payment;
+    double interest_rate, principle, monthly_payment, total_paid;
     int loan_term;
 
     display_greeting();
@@ -27,9 +28,19 @@ int main()
 
     // Now, calculate the monthly payment.
     monthly_payment = calculate_monthly_payment(principle, interest_rate, loan_term);
+    total_paid = calculate_total_paid(monthly_payment, loan_term);
+
     printf("\nCongratulations! You're giving your life away for $%.2lf/month!\n", monthly_payment);
+    printf("You will pay $%.2lf on this loan, ", total_paid);
+    printf("$%.2lf more than if you just paid in cash.\n", total_paid - principle);
 
     return 0;
+}
+
+double calculate_total_paid(double monthly_payment, int years)
+{
+    int months = years * 12;
+    return monthly_payment * months;
 }
 
 /**
@@ -38,9 +49,15 @@ int main()
 double calculate_monthly_payment(double principle, double interest_rate, int loan_term)
 {
     int months = get_months_from_years(loan_term);
-    double percent_interest = calculate_interest_percent(interest_rate);
+    double percent_interest = calculate_interest_percent(interest_rate), monthly_payment = 0.0;
 
-    return (percent_interest + percent_interest / pow(1 + percent_interest, months - 1)) * principle;
+    monthly_payment = pow(1+percent_interest, months);
+    monthly_payment = monthly_payment - 1;
+    monthly_payment = percent_interest / monthly_payment;
+    monthly_payment = percent_interest + monthly_payment;
+    monthly_payment = monthly_payment * principle;
+
+    return monthly_payment;
 }
 
 /**
