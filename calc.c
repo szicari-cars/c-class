9f0324cfe350033a00919f9885360ae17a4644a8
Sean Zicari <szicari@cars.com>
2012-10-11 09:14:30 -0700

    Calculator works with non-floating point values only.

--- calc.c
+++ calc.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool process_input(char input, double *result);
-void clear_buffer();
+bool process_input(char, double *);
+bool validate_operand(double operand);
+double do_calculation(double result, double operand, char operation);
+double get_number_from_buffer();
+void clear_input_buffer();
+void show_help_text();
 
 int main()
 {
@@ -10,23 +14,47 @@ int main()
     double result = 0.0;
 
     printf("Calculator is on.\n");
+    show_help_text();
 
-    while (keep_calculating)
+    do
     {
         printf("Please enter your input (%.2f): ", result);
-        keep_calculating = process_input(getchar(), &result);
     }
+    while (process_input(getchar(), &result));
 }
 
+void show_help_text()
+{
+    printf("\n");
+    printf("Valid inputs\n");
+    printf("------------\n");
+    printf("h -- This help text\n");
+    printf("r -- get current result and clear calculator\n");
+    printf("q -- quit calculator");
+    printf("+num -- Add num to current result\n");
+    printf("-num -- subtract num from current result\n");
+    printf("/num -- divide current result by num\n");
+    printf("*num -- multiply current result by num\n");
+    printf("\n");
+}
+
+/**
+ * Examines the first letter of input and takes action. This works because
+ * the first letter is always a command, whether it be 'r', 'q' or a math
+ * operation to perform. In certain cases, we will continue getting input
+ * from the stdin buffer. In other cases, we will act immediately. Always,
+ * we will clear the buffer before continuing.
+ */
 bool process_input(char input, double *result)
 {
-    bool keep_calculating = true;
+    bool keep_calculating = true, clear_buffer = true;
+    double operand = 0.0;
 
     switch (input)
     {
         case 'h':
         case 'H':
-            printf("Help goes here.\n");
+            show_help_text();
             break;
 
         case 'r':
@@ -35,23 +63,58 @@ bool process_input(char input, double *result)
             *result = 0.0;
             break;
 
+        case '+':
+        case '-':
+        case '/':
+        case '*':
+            operand = get_number_from_buffer();
+            *result = do_calculation(*result, operand, input);
+            printf("result %c %.2f = %.2f.\n", input, operand, *result);
+            // The buffer is cleared as a side effect of fetching the number
+            clear_buffer = false;
+            break;
+
         case 'q':
         case 'Q':
             keep_calculating = false;
             break;
 
-        case '\n':
-            break;
-
         default:
-            printf("UnknownOperatorException is thrown.\n");
+            printf("'%c' is an unknown operation.\n", input);
     }
+
+    if (clear_buffer) clear_input_buffer();
 
     return keep_calculating;
 }
 
-void clear_buffer()
+void clear_input_buffer()
 {
     while (getchar() != '\n');
+}
+
+double do_calculation(double result, double operand, char operation)
+{
+    if (operation == '+') result += operand;
+    if (operation == '-') result -= operand;
+    if (operation == '/') result /= operand;
+    if (operation == '*') result *= operand;
+
+    return result;
+}
+
+double get_number_from_buffer()
+{
+    int digit = 0;
+    double num = 0.0;
+    char ch;
+
+    while ((ch = getchar()) != '\n' && ch != EOF)
+    {
+        digit = ch - '0';
+        num = (num * 10) + digit;
+    }
+
+    return num;
 }
 
