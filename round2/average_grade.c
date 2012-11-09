ff7226df13353c34add90035d4bf0f225a660790
Sean Zicari <sean.zicari@gmail.com>
2012-11-09 12:08:16 -0800

    What am I doing wrong?!

--- round2/average_grade.c
+++ round2/average_grade.c
@@ -5,57 +5,54 @@
 
 int get_number_of_students();
 void clear_input_buffer();
-void initialize_student_data_arrays(char *, float *, int);
-void get_student_data(int, char *, float *);
+void get_student_data(int);
+
+struct student {
+    char *name;
+    float test_grades[];
+};
 
 int main()
 {
-	int num_students = get_number_of_students();
-	char * student_names;
-	float * student_grades;
+    int num_students = get_number_of_students();
 
-	if (num_students > 0)
-	{
-		initialize_student_data_arrays(student_names, student_grades, num_students);
-		get_student_data(num_students, student_names, student_grades);
-	}
+    if (num_students > 0)
+    {
+        get_student_data(num_students);
+    }
 
-	return 0;
+    return 0;
 }
 
-void get_student_data(int num_students, char * student_names, float * student_grades)
+void get_student_data(int num_students)
 {
-	int i = 0;
+    int i = 0;
+    char name[300];
 
-	for (i = 0; i < num_students; i++)
-	{
-		printf("Enter name of student %d: ", i);
-		gets(student_names[i]);
-	}
-}
-
-void initialize_student_data_arrays(char * student_names, float * student_grades, int num_students)
-{
-	student_names = (char *) malloc(num_students * sizeof(char));
-	student_grades = (float *) malloc(num_students * sizeof(float));
+    for (i = 0; i < num_students; i++)
+    {
+        printf("Enter name of student %d: ", i+1);
+        fgets(name, sizeof(name), stdin);
+        printf("\nName is %s.\n", name);
+    }
 }
 
 int get_number_of_students()
 {
-	int num_students = 0, attempt = 0, max_attempts = 5;
+    int num_students = 0, attempt = 0, max_attempts = 5;
 
-	while (num_students <= 0 && attempt < max_attempts)
-	{
-		printf("How many students are we tracking grades for? ");
-		scanf("%d", &num_students);
-		attempt += 1;
-	}
+    while (num_students <= 0 && attempt < max_attempts)
+    {
+        printf("How many students are we tracking grades for? ");
+        scanf("%d", &num_students);
+        attempt += 1;
+    }
 
-	return num_students;
+    return num_students;
 }
 
 void clear_input_buffer()
 {
-	while (getchar() != '\n');
+    while (getchar() != '\n');
 }
 
