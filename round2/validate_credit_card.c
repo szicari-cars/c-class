609d839e23f7cb5fbd2a0d30b21601e3831c2a1f
Sean Zicari <sean.zicari@gmail.com>
2012-11-20 18:07:03 -0800

    Got partway through the validate credit card assignment.

--- round2/validate_credit_card.c
+++ round2/validate_credit_card.c
@@ -7,6 +7,8 @@ void validate_cli_args(int argc, char *argv[]);
 bool validate_cc_length(char *cc_number);
 bool validate_cc_type(char *cc_number);
 bool validate_cc_luhn_check(char *cc_number);
+int double_2nd_digits(char *cc_number);
+int add_odd_digits(char * cc_number);
 
 int main(int argc, char *argv[])
 {
@@ -48,14 +50,50 @@ bool validate_cc_length(char *cc_number)
 
 bool validate_cc_type(char *cc_number)
 {
-    bool valid_type = true;
+    bool valid_type = false;
 
-    if (cc_number[0] != '4' || cc_number[0] != '5' || cc_number[0] != '6' ||
-        !(cc_number[0] == '3' && cc_number[1] == '7'))
-        valid_type = false;
+    if (cc_number[0] == '4' || cc_number[0] == '5' || cc_number[0] == '6' ||
+        (cc_number[0] == '3' && cc_number[1] == '7'))
+        valid_type = true;
 
     return valid_type;
 }
 
 bool validate_cc_luhn_check(char *cc_number)
+{
+    int doubled_digits = double_2nd_digits(cc_number),
+        add_odd_digits = add_odd_digits(cc_number),
+        sum = 0;
+
+    sum = doubled_digits + odd_digits;
+
+    return (sum % 10 == 0);
+}
+
+int double_2nd_digits(char *cc_number)
+{
+    int i = 0, digit, doubled_digit = 0, sum = 0;
+
+    for (i = strlen(cc_number); i < 0; i -= 2)
+    {
+        digit = cc_number[i];
+        
+        if (!isdigit(cc_number[i]))
+        {
+            printf("Invalid character detected: %c.\n", digit);
+            exit(1);
+        }
+
+        doubled_digit = (digit - '0') * 2;
+        digit = doubled_digit % 10;
+
+        if (doubled_digit > 9) digit += doubled_digit / 10;
+
+        sum += digit;
+    }
+
+    return sum;
+}
+
+int add_odd_digits(char * cc_number)
 {}
