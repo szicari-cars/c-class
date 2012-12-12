44f83fece009b6f4806c0ba25f8b48ea3a23a2a3
Sean Zicari <szicari@cars.com>
2012-12-11 17:06:50 -0800

    Finished problem 12 on "page 254".

---
+++ round2/fill_array_with_ints.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+void fill_array_with_ints(int * array, size_t array_size);
+void print_array_with_ints(int * array, size_t array_size);
+
+int main()
+{
+    int array[50];
+    size_t array_size = sizeof array / sizeof(array[0]);
+
+    srand(time(NULL));
+    fill_array_with_ints(array, array_size);
+    print_array_with_ints(array, array_size);
+}
+
+void fill_array_with_ints(int * array, size_t array_size)
+{
+    int i = 0;
+    int * array_pointer = array;
+
+    for (i = 0; i < array_size; i++)
+        array_pointer[i] = rand() % 10;
+}
+
+void print_array_with_ints(int * array, size_t array_size)
+{
+    int i = 0;
+    int * array_pointer = array;
+
+    for (i = 0; i < array_size; i++)
+        printf("Item %d: %d\n", i, array_pointer[i]);
+}
